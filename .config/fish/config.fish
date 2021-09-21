df4622b5bb330c0b981ceef43b26df37caf1ad71
drizzlt <zimon.a7@gmail.com>
2021-09-21 17:16:56 +0200

    Create config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+### run always on start ###
+#do_at_init
+. ~/.config/fish/aliases.fish
+
+### abbreviations ###
+#abbr s 'subl'						# for sublime edit
+abbr s 'flatpak run com.sublimetext.three'		# for sublime edit
+abbr fcf 'cd ~/.config/fish'				# for fish config
+abbr open 'xdg-open'
